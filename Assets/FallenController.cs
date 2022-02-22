eff37498c866ea1a5f029496c0551dde96e3ba77
0 <84623510+Cateye0326@users.noreply.github.com>
2022-02-22 19:10:33 +0900

    横に動くとか

---
+++ Assets/FallenController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenController : MonoBehaviour
+{
+
+    [SerializeField]
+    [Tooltip("Circle")]
+    private GameObject createPrefab;
+    [SerializeField]
+    [Tooltip("-6,4.5")]
+    private Transform rangeA;
+    [SerializeField]
+    [Tooltip("5.6,4.5")]
+    private Transform rangeB;
+
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time + Time.deltaTime;
+
+        if (time > 2.0f)
+        {
+            float x = Random.Range(rangeA.position.x, rangeB.position.x);
+            float y = Random.Range(rangeA.position.y, rangeB.position.y);
+
+            _ = Instantiate(createPrefab, new Vector2(x, y), createPrefab.transform.rotation);
+
+            time = 0f;
+        }
+    }
+}
